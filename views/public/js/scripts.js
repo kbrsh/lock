c8b1a7293520f7b072a3b5fbce9fa6fb0910cf8b
fsafdsf <lol@f.sf.sf>
2016-05-20 04:24:43 +0000

    loader

--- views/public/js/scripts.js
+++ views/public/js/scripts.js
@@ -1,2 +1,13 @@
-new Clipboard('.btn');
+$('.notFooter').hide();
+document.write("<div class='centered text-center' id='loading'><h1>Del</h1><br><p>Simple Messages that Self Destruct</p><div class='loader'></div></div>");
 
+$(document).ready(function() {
+   setTimeout(function() {
+    $('#loading').addClass("animate");
+    $("#loading").addClass("fadeOut");
+    setTimeout(function() {
+      $('#loading').hide();
+      $('.notFooter').show();
+  }, 500);
+}, 1000); 
+});
