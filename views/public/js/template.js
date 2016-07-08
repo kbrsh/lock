41a1bfd172b80e23d47927cd08b8249d4a42cdf3
kingsofcoding <simplecooldude1@gmail.com>
2016-07-08 18:01:28 +0000

    counter

--- views/public/js/template.js
+++ views/public/js/template.js
@@ -1,3 +1,11 @@
+var count = 5;
+document.getElementById("counter").innerHTML = count;
+
 setTimeout(function() {
     document.getElementById("overlay").style.display = '';
 }, 5000);
+
+setInterval(function() {
+    count--;
+    document.getElementById("counter").innerHTML = count;
+}, 1000);
